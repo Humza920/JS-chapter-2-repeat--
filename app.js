6b8eb61e615163803056bc2d268eb4af9069aaf2
Humza920 <humza.rehman.779@gmail.com>
2024-04-03 14:09:56 -0700

    HALF

---
+++ app.js
@@ -0,0 +1,21 @@
+// JS CHAPTER 2
+
+// QUESTION 1
+// var username 
+// ISS SAWAL MAI KHALI VARIABLE DECLARE KARNA THA
+
+// QUESTION 2
+// var myName = "Humza" + " " + "Rehman"
+
+// QUESTION 3
+// var message = "HELLO WORLD"
+// alert(message)
+
+// QUESTION 4
+// var fullName = "Humza" + " " + "Rehman"
+// var age = 17
+// var skill = "MERN" + " " + "STACK" + " " + "DEVELOPER"
+
+// alert(fullName)
+// alert(age)
+// alert(skill)
